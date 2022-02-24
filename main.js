79333e39caa15d143d6134d886afb333d3699431
jillannette <99822166+jillannette@users.noreply.github.com>
2022-02-24 16:40:15 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+let registeredEarly = true; 
+let runnerAge = 58
+
+
+
+if (runnerAge >18 && registeredEarly) {
+    console.log(`"Your race number is ${raceNumber += 1000} and your race time is 9:30 a.m."`)
+} else if (runnerAge <18) {
+    console.log(`"Youth registrants run at 12:30 p.m.; your race number is ${raceNumber}".`)
+} else (runnerAge >18) 
+    console.log(`"Late adults race at 11:00 a.m.; your race number is ${raceNumber += 1000}."`)
